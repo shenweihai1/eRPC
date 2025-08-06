ee7f8bb169ad907da79179f8e6912b15f623d35f
shenweihai1 <shenweihai1@gmail.com>
2025-08-06 02:38:30 +0000

    No need to manually compile erpc: change CMAKE_SOURCE_DIR to CMAKE_CURRENT_SOURCE_DIR so that Mako Cmakelists.txt can find the correct path

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-make clean
+rm -rf CMakeFiles cmake_install.cmake CMakeCache.txt
 #cmake . -DTRANSPORT=dpdk -DAZURE=off -DPERF=ON -DLOG_LEVEL=none
 #cmake . -DTRANSPORT=infiniband -DROCE=on -DPERF=ON
 cmake . -DTRANSPORT=fake -DROCE=off -DPERF=off
